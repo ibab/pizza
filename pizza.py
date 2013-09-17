cffb87ba0098f30e2fd8532af51df41e4bfd44ae
Igor Babuschkin <igor@babuschk.in>
2013-09-17 10:53:55 +0200

    Implement updating over Websocket

--- pizza.py
+++ pizza.py
@@ -1,11 +1,17 @@
+# encoding: UTF-8
 
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+     abort, render_template, flash, jsonify
 from contextlib import closing
+
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler
+import gevent.queue
 
 import re
 import time
+import json
 
 DATABASE = './pizza.sqlite3'
 DEBUG = True
@@ -17,6 +23,11 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 app.config.from_envvar('PIZZA_SETTINGS', silent=True)
+
+host, port='localhost', 5000
+
+def cents_to_euros(cents):
+    return '{},{:02d} €'.format(int(cents / 100), cents % 100)
 
 def init_db():
     with closing(connect_db()) as db:
@@ -43,8 +54,17 @@ def show_entries():
     entries = [dict(pid=row[0], description=row[1], author=row[2], price=row[3], paid=row[4]) for row in cur.fetchall()]
     for e in entries:
         amount = e['price']
-        e['price'] = '{},{:02d} €'.format(int(amount / 100), amount % 100)
+        e['price'] = cents_to_euros(amount)
     return render_template('show_entries.html', entries=entries)
+
+@app.route('/get/entries')
+def get_entries():
+    cur = g.db.execute('select id, description, author, price, paid from entries order by id desc')
+    entries = [dict(pid=row[0], description=row[1], author=row[2], price=row[3], paid=row[4]) for row in cur.fetchall()]
+    for e in entries:
+        amount = e['price']
+        e['price'] = cents_to_euros(amount)
+    return json.dumps(entries)
 
 @app.route('/edit/<int:pid>/<action>', methods=['POST'])
 def edit_entry(pid, action):
@@ -52,8 +72,12 @@ def edit_entry(pid, action):
         cur = g.db.execute('SELECT paid from entries WHERE id=?', [pid])
         paid = cur.fetchone()[0]
         g.db.execute('UPDATE entries SET paid=? WHERE id=?', [not paid, pid])
+        for q in queues:
+            q.put(('toggle_paid', pid))
     if action == 'delete':
         g.db.execute('DELETE FROM entries WHERE id=?', [pid])
+        for q in queues:
+            q.put(('delete_entry', pid))
     g.db.commit()
     return redirect(url_for('show_entries'))
 
@@ -64,11 +88,11 @@ def add_entry():
     price = re.findall('(\d+)(?:[,.](\d))?\s*(?:€|E)?', request.form['price'])
 
     if not description:
-        flash('Please provide a description', 'error')
+        return jsonify(msg='Please provide a description', type='error')
     elif not author:
-        flash('Please provide your name', 'error')
+        return jsonify(msg='Please provide your name', type='error')
     elif not price:
-        flash('Price must be formed like this: 3.14', 'error')
+        return jsonify(msg='Price must be formed like this: 3.14', type='error')
     else:
         value = price[0]
         price = int(value[0]) * 100
@@ -77,11 +101,38 @@ def add_entry():
                 price += int(value[1]) * 10
             else:
                 price += int(value[1])
-        g.db.execute('insert into entries (description, author, price, paid) values (?, ?, ?, ?)',
+        csr = g.db.execute('insert into entries (description, author, price, paid) values (?, ?, ?, ?)',
                 [request.form['description'], request.form['author'], price, False])
+        pid = csr.lastrowid
         g.db.commit()
         flash('A new entry has been posted', 'success')
-    return redirect(url_for('show_entries'))
+        data = {'description': description,
+                'author': author,
+                'price': cents_to_euros(price),
+                'paid': 0,
+                'pid': pid} 
+                
+        for q in queues:
+            q.put(('create_entry', data))
+    return jsonify(msg='New entry added', type='success')
+
+def wsgi_app(environ, start_response):  
+    path = environ["PATH_INFO"]  
+    if path == "/websocket":  
+        handle_websocket(environ["wsgi.websocket"])
+    else:  
+        return app(environ, start_response)
+
+queues = []
+
+def handle_websocket(ws):
+    q = gevent.queue.Queue()
+    queues.append(q)
+    while True:
+        type, data = q.get()
+        ws.send(json.dumps({'type': type, 'data': data}))
 
 if __name__ == '__main__':
-    app.run()
+    http_server = WSGIServer((host, port), wsgi_app, handler_class=WebSocketHandler)
+    print('Server started at %s:%s'%(host,port))
+    http_server.serve_forever()
--- static/site.js
+++ static/site.js
@@ -1,5 +1,75 @@
 
-pizza_toggle_paid = function(pid) {
+display_nopizzas = function() {
+    if($.trim($("#orders").html())=='') {
+        li = $('<li>', {id: 'noentry', class: 'list-group-item'}).append(
+               $('<em>').text('No pizzas so far. Why not add one?')
+              )
+        li.slideDown();
+        li.css('display', 'block');
+        $("#orders").append(li);
+    } else {
+        $('#noentry').slideUp(function() {
+            $('#noentry').remove();
+        });
+    }
+}
+
+render_initial_entries = function() {
+    $.getJSON('/get/entries', function(entries) {
+        pizza_entries = entries
+        orders = $("#orders")
+        for (var i=0; i<entries.length; i++) {
+            create_entry(entries[i]);
+        }
+        display_nopizzas();
+    });
+}
+
+create_entry = function(entry) {
+    description = entry['description'];
+    pid = entry['pid'];
+    console.log(pid);
+    price = entry['price'];
+    author = entry['author'];
+    paid = entry['paid'];
+
+    var li = $('<li>', {id: 'entry'+pid, class: 'list-group-item'});
+    var span = $('<span>', {class: 'badge pull-left'});
+    span.text(price);
+    li.append(span);
+    li.append(description + ' (' + author + ')');
+
+    button1 = $('<button>', {id: 'paid'+pid, class: 'btn btn-default btn-xs'}).append('bezahlt');
+    button2 = $('<button>', {class: 'btn btn-default btn-xs'}).append('löschen');
+    if (paid) {
+        button1.addClass('active btn-success');
+    }
+
+    button1.click(pid, function(pid) {
+        $.post('/edit/' + pid.data + '/toggle_paid');
+    });
+    button2.click(pid, function(pid) {
+        $.post('/edit/' + pid.data + '/delete');
+    });
+
+    btngrp = $('<div>', {class: 'btn-group pull-right'}).append(button1, button2);
+    li.slideDown();
+    li.css('display', 'block');
+    li.append(btngrp);
+    $('#orders').append(li);
+    display_nopizzas();
+}
+
+
+delete_entry = function(pid) {
+    entry = $('#entry' + pid);
+    entry.slideUp(400, function() {
+        entry.remove();
+        display_nopizzas();
+    });
+}
+
+toggle_paid = function(pid) {
     btn = $('#paid' + pid);
     if (btn.hasClass('active')) {
         btn.removeClass('active');
@@ -8,11 +78,49 @@ pizza_toggle_paid = function(pid) {
         btn.addClass('active');
         btn.addClass('btn-success');
     }
-    $.post('/edit/' + pid + '/toggle_paid')
 }
 
-pizza_delete = function(pid) {
-    entry = $('#entry' + pid);
-    entry.remove()
-    $.post('/edit/' + pid + '/delete')
-}
+render_initial_entries();
+
+$('#addpizza').on('submit', function(event) {
+    event.preventDefault();
+    $.post('/add', $('#addpizza').serialize(), function(data) {
+        type = data.type;
+        msg = data.msg
+        var div = undefined
+        if (type == 'error') {
+            div = $('<div>', {class: 'alert alert-danger alert-dismissable'});
+            div.html('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>')
+            div.append($('<strong>').text('Failure! '));
+            div.append(msg);
+        } else if (type == 'success') {
+            div = $('<div>', {class: 'alert alert-success alert-dismissable'});
+            div.html('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>')
+            div.append($('<strong>').text('Success! '));
+            div.append(msg);
+        }
+        div.slideDown();
+        $('#main-panel').before(div);
+    });
+});
+
+(function(){
+    ws = new WebSocket("ws://localhost:5000/websocket");
+    //ws.addEventListener('open', function(e){
+    //});
+    //ws.onerror = function(){
+    //    setTimeout(function(){start(websocketServerLocation)}, 1000);
+    //};
+    ws.addEventListener('message', function(e) {
+        evt = JSON.parse(e.data);
+        if (evt['type'] == 'create_entry') {
+            create_entry(evt['data']);
+        }
+        if (evt['type'] == 'delete_entry') {
+            delete_entry(evt['data']);
+        }
+        if (evt['type'] == 'toggle_paid') {
+            toggle_paid(evt['data']);
+        }
+    });
+})();
